d3b18aa97a3f14f5003dcd6017cb92d01233cbc8
abhinavthakkallapally <86998295+abhinavthakkallapally@users.noreply.github.com>
2023-07-06 18:54:16 +0530

    Update streamlit_app.py
    Added default fruits in multi-select.

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,5 @@ import pandas as pd
 f_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 f_list = f_list.set_index('Fruit')
 
-streamlit.multiselect("Pick some fruits:",list(f_list.index))
+streamlit.multiselect("Pick some fruits:",list(f_list.index),['Avocado','Strawberries'])
 streamlit.dataframe(f_list)

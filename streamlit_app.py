5458489fb8068bf06ecb0843a0f368117fe858c3
abhinavthakkallapally <86998295+abhinavthakkallapally@users.noreply.github.com>
2023-07-06 18:36:26 +0530

    Update streamlit_app.py
    Added Header and 4 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("First Page!!")
+streamlit.header("Breakfast Menu..")
+streamlit.text("🥣 One")
+streamlit.text(" 🥗 Two")
+streamlit.text("🐔 Three")
+streamlit.text("🥑🍞 Four")
